228d694a1bb60b0dcade15bd6744fd3ccdd40be3
Deepanshumethi <deepanshumethi123@gmail.com>
2024-06-24 00:43:28 +0530

    change

--- build_files.sh
+++ build_files.sh
@@ -4,13 +4,10 @@
 set -e
 
 # Collect static files
-python manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput
 
 # Apply database migrations
-python manage.py migrate
+python3 manage.py migrate
 
-
-pip install -r requirements.txt
-python3.9 manage.py collectstatic
 
 

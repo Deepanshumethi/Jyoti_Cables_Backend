d682586b24b53c85755af142b290b586eab9e114
Deepanshumethi <deepanshumethi123@gmail.com>
2024-06-24 00:39:01 +0530

    change

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,16 @@
+#!/bin/sh
+
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Apply database migrations
+python manage.py migrate
+
+
 pip install -r requirements.txt
 python3.9 manage.py collectstatic
+
+
